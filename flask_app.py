19ed8556f12c722b1612eaa19dc62fcd520b07e9
arsha <arshababu333@gmail.com>
2025-10-08 13:18:26 -0400

    Add environment config, GitHub workflows, and Flask app

---
+++ flask_app.py
@@ -0,0 +1,19 @@
+
+from app import create_app
+from app.extensions import db
+
+
+app = create_app('ProductionConfig')
+
+with app.app_context():
+    #db.drop_all() 
+    db.create_all() 
+    
+"""if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+""" 
+ 
+
+
+
+        
